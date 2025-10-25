96312fab3c9461a4e0edc4c9a28b890baf41b537
Jake6654 <ugw2ad@virginia.edu>
2025-10-25 12:14:58 -0400

    Add NavBar

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class page extends Component {
+  static propTypes = {};
+
+  render() {
+    return <div>asda</div>;
+  }
+}
+
+export default page;
