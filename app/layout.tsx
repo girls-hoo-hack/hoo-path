268f5240365b7794895e26e246a2f87f42cc6606
hiimstevejin <jingle5695@gmail.com>
2025-10-25 20:39:36 -0400

    add proxy

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
       <Toaster richColors />
+      </body>
     </html>
   );
 }
